e23779dccf5e87083ec12f8234718c25065345f4
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2025-03-10 09:21:04 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,16 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
 
+  // If the user is not authenticated and tries to access a protected route, redirect to login
   if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    if (request.nextUrl.pathname !== "/login") {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  } else {
+    // If the user is authenticated and tries to access the login page, redirect to the dashboard
+    if (request.nextUrl.pathname === "/login") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 
   return NextResponse.next();
@@ -21,6 +29,6 @@ export const config = {
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      * - login (login page)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
